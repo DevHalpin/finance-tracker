40403a79bcd05d517e417e1b3016650144e6953c
David Halpin <devhalpin86@gmail.com>
2025-06-01 22:17:43 -0700

    create serializer and view for accounts

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, get_accounts
 
 urlpatterns = [
     path('hello/', hello_world),
+    path('accounts/', get_accounts),
 ]
