88744d2921d8afdb452a432b069e7ff5159cfa9a
David Halpin <devhalpin86@gmail.com>
2025-06-01 22:17:43 -0700

    create serializer and view for accounts

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Account(models.Model):
     name = models.CharField(max_length=100)
     user_id = models.CharField(max_length=100)
+    plaid_id = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Account
+from .serializers import AccountSerializer
 
 @api_view(['GET'])
 def hello_world(request):
     return Response({"message": "Hello from Django!"})
+
+@api_view(['GET'])
+def get_accounts(request):
+    accounts = Account.objects.all()
+    serializer = AccountSerializer(accounts, many=True)
+    return Response({ "accounts": serializer.data })
