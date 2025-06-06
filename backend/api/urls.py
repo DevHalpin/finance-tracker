5ee31a5d70aced13c2a1aa44ed7081cb9d334795
David Halpin <devhalpin86@gmail.com>
2025-06-05 19:49:08 -0700

    refactor views to allow both get and post to /accounts

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import hello_world, get_accounts
+from .views import hello_world, accounts
 
 urlpatterns = [
     path('hello/', hello_world),
-    path('accounts/', get_accounts),
+    path('accounts/', accounts),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -7,16 +7,17 @@ from .serializers import AccountSerializer
 def hello_world(request):
     return Response({"message": "Hello from Django!"})
 
-@api_view(['GET'])
-def get_accounts(request):
-    accounts = Account.objects.filter(user=request.user)
-    serializer = AccountSerializer(accounts, many=True)
-    return Response({ "accounts": serializer.data })
+@api_view(['GET', 'POST'])
+def accounts(request):
+    if request.method == 'GET':
+        accounts = Account.objects.filter(user=request.user)
+        serializer = AccountSerializer(accounts, many=True)
+        return Response({ "accounts": serializer.data }, status=200)
 
-@api_view(['POST'])
-def create_account(request):
-    serializer = AccountSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save(user=request.user)
-        return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
+    if request.method == 'POST':
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
