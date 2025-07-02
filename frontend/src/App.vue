ef343da01d81e27c2b2978cc3e96e8ae4fb4668e
David Halpin <devhalpin86@gmail.com>
2025-07-01 18:24:22 -0700

    clean up App.vue by creating a Layout file for drawers

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,11 @@
 <template>
-    <v-app>
-        <NewAccountDrawer v-model="newAccountOpen" />
-        <EditAccountDrawer v-model="editAccountOpen" />
-        <EditCategoryDrawer v-model="editCategoryOpen" />
-        <NewCategoryDrawer v-model="newCategoryOpen" />
-        <HeaderView />
+    <AppLayout>
         <router-view />
-    </v-app>
+    </AppLayout>
 </template>
 
 <script setup lang="ts">
-import HeaderView from './components/HeaderView.vue';
-import NewAccountDrawer from './features/accounts/components/NewAccount.vue';
-import EditAccountDrawer from './features/accounts/components/EditAccount.vue';
-import EditCategoryDrawer from './features/categories/components/EditCategory.vue';
-import NewCategoryDrawer from './features/categories/components/NewCategory.vue';
-import { useNewAccount } from './features/accounts/hooks/useNewAccount';
-import { useOpenAccount } from './features/accounts/hooks/useOpenAccount';
-import { useNewCategory } from './features/categories/hooks/useNewCategory';
-import { useOpenCategory } from './features/categories/hooks/useOpenCategory';
-const { drawerOpen: newAccountOpen } = useNewAccount();
-const { drawerOpen: editAccountOpen } = useOpenAccount();
-const { drawerOpen: newCategoryOpen } = useNewCategory();
-const { drawerOpen: editCategoryOpen } = useOpenCategory();
-
-
+import AppLayout from './layouts/AppLayout.vue';
 </script>
 
 <style>
---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <NewAccountDrawer v-model="newAccountOpen" />
+        <EditAccountDrawer v-model="editAccountOpen" />
+        <EditCategoryDrawer v-model="editCategoryOpen" />
+        <NewCategoryDrawer v-model="newCategoryOpen" />
+        <HeaderView />
+        <router-view />
+    </v-app>
+</template>
+
+<script setup lang="ts">
+import HeaderView from '../components/HeaderView.vue';
+import NewAccountDrawer from '../features/accounts/components/NewAccount.vue';
+import EditAccountDrawer from '../features/accounts/components/EditAccount.vue';
+import EditCategoryDrawer from '../features/categories/components/EditCategory.vue';
+import NewCategoryDrawer from '../features/categories/components/NewCategory.vue';
+import { useNewAccount } from '../features/accounts/hooks/useNewAccount';
+import { useOpenAccount } from '../features/accounts/hooks/useOpenAccount';
+import { useNewCategory } from '../features/categories/hooks/useNewCategory';
+import { useOpenCategory } from '../features/categories/hooks/useOpenCategory';
+const { drawerOpen: newAccountOpen } = useNewAccount();
+const { drawerOpen: editAccountOpen } = useOpenAccount();
+const { drawerOpen: newCategoryOpen } = useNewCategory();
+const { drawerOpen: editCategoryOpen } = useOpenCategory();
+</script>
