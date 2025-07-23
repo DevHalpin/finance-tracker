72e6a4ca5eeba866664b4dd79210bac7ca2bb030
David Halpin <devhalpin86@gmail.com>
2025-07-22 23:20:22 -0700

    add function to format currency

--- frontend/src/libs/utils.ts
+++ frontend/src/libs/utils.ts
@@ -5,3 +5,10 @@ export function convertAmountToMilliunits(amount: number): number {
 export function convertMilliunitsToAmount(milliunits: number): number {
   return milliunits / 1000
 }
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount)
+}
